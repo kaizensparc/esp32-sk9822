d380595c1977aa04d604484fd332fb5d0c71cf8c
Clément Decoodt <clement@decoodt.eu>
2019-05-23 23:21:21 +0200

    Add support for animations

--- main/led.c
+++ main/led.c
@@ -6,6 +6,9 @@
 #include "freertos/task.h"
 
 #include "mqtt.h"
+#include "queues.h"
+
+static const char* TAG = "LED";
 
 static void animate_4() {
     for (int i = 0; i < 1; i++) {
@@ -80,20 +83,48 @@ static void animate_1() {
     }
 }
 
+static void (*animate[])(void) = {animate_1, animate_2, animate_3, animate_4};
+
+static enum led_animation_name current_animation = ANIMATE_1;
+
+static uint8_t anim_idx = 0;
+// Simple routine to change animation index
+#define ANIMATION_CHANGER_STACK_SIZE 2048
+StaticTask_t animation_changer_buffer;
+StackType_t animation_changer_stack[ANIMATION_CHANGER_STACK_SIZE];
+static void animation_changer(void* pvParameter) {
+    while (1) {
+        if (xQueueReceive(dispatcher_queues[QUEUE_ANIM], (void*)&anim_idx,
+                          portMAX_DELAY) != pdTRUE) {
+            ESP_LOGI(TAG, "Queue is not available, ignoring message");
+            continue;
+        }
+        if (anim_idx >= LED_ANIMATION_NAME_LENGTH) {
+            continue;
+        }
+        ESP_LOGI(TAG, "Switching to animation %d", anim_idx);
+        current_animation = anim_idx;
+    }
+}
+
 // Simple routine to generate some patterns and send them to the LED Strip.
-#define STACK_SIZE 4096
+#define ANIMATE_TASK_STACK_SIZE 4096
 StaticTask_t animate_task_buffer;
-StackType_t animate_task_stack[STACK_SIZE];
+StackType_t animate_task_stack[ANIMATE_TASK_STACK_SIZE];
 static void animate_task(void* pvParameter) {
     while (1) {
-        animate_1();
+        (*animate[current_animation])();
     }
 }
 
 void led_init(apa102_spi_device_t* device) {
     apa102_init(device);
 
-    xTaskCreateStatic(&animate_task, "animate_task", STACK_SIZE, NULL,
-                      tskIDLE_PRIORITY + 1, animate_task_stack,
+    xTaskCreateStatic(&animate_task, "animate_task", ANIMATE_TASK_STACK_SIZE,
+                      NULL, tskIDLE_PRIORITY + 1, animate_task_stack,
                       &animate_task_buffer);
+
+    xTaskCreateStatic(&animation_changer, "animation_changer",
+                      ANIMATION_CHANGER_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1,
+                      animation_changer_stack, &animation_changer_buffer);
 }
--- main/led.h
+++ main/led.h
@@ -2,4 +2,13 @@
 
 #include "apa102.h"
 
+enum led_animation_name {
+    ANIMATE_1,
+    ANIMATE_2,
+    ANIMATE_3,
+    ANIMATE_4,
+};
+
+#define LED_ANIMATION_NAME_LENGTH (ANIMATE_4 + 1)
+
 void led_init(apa102_spi_device_t *device);
